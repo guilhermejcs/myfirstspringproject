da8a8f89371adc34b8984cdd48a25b8744735e06
Guilherme Souza <guilhermejcamargos@gmail.com>
2020-09-19 00:59:42 -0300

    products memory

--- src/main/java/com/devsuperior/myfirstproject/entities/Product.java
+++ src/main/java/com/devsuperior/myfirstproject/entities/Product.java
@@ -4,26 +4,25 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-public class Category implements Serializable{
-	
+public class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
-
+	
 	private Long id;
 	private String name;
+	private Double price;
 	
-	@JsonIgnore
-	private List<Product> products = new ArrayList<>();
-	
-	public Category() {
-		
+	private Category category;
+
+	public Product() {
+		super();
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, Double price, Category category) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -42,10 +41,22 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
-	public List<Product> getProducts() {
-		return products;
+	public Double getPrice() {
+		return price;
 	}
-	
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -62,7 +73,7 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -70,6 +81,7 @@ public class Category implements Serializable{
 			return false;
 		return true;
 	}
+	
 	
 	
 	
